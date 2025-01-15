0da68c79fc7eea77a730ed2586d3ff701d0e3bd4
monsterTroll <monstroll111@gmail.com>
2025-01-15 18:12:01 +0800

    finished import sass

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //開發中dev
+  base: process.env.NODE_ENV === "production" ? "/hex-react-gh-pages/" : "/",
+  //產品路徑 build
   plugins: [react()],
 })
